f594eed82d468cbe446fe3bd025e3d80b49f5fe2
Kateryna Tkachenko <Tkachenko.city@gmail.com>
2024-03-05 19:17:28 +0200

    removed extra console.log

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,7 +14,6 @@ export const App = () => {
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    console.log(console.log('didMount'));
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
